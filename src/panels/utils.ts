5c14804dd65c740c57ac71f09bf4ea6a8c896831
CosmoLau <cosmolau@qq.com>
2026-05-13 17:02:26 +0800

    feat: 新增绝对路径转 url 工具函数

--- src/panels/utils.ts
+++ src/panels/utils.ts
@@ -8,3 +8,18 @@ import path from 'node:path';
 export function getProjectPath(relativePath = "") {
     return path.join(Editor.Project.path, relativePath);
 }
+
+/**
+ * 绝对路径转换为 Cocos 项目的 url
+ * @param absolutePath 绝对路径
+ * @returns Cocos 项目的 url，例如：`db://assets/`
+ */
+export function absoluteToUrl(absolutePath: string) {
+    let relativePath = path.relative(getProjectPath(), absolutePath);
+    if (relativePath == '' || relativePath.startsWith('..') || path.isAbsolute(relativePath)) {
+        console.error(`${absolutePath} 不在项目路径 ${getProjectPath()} 下，请检查路径`);
+        return;
+    }
+    let url = "db://" + relativePath.split(path.sep).join("/");
+    return url;
+}
